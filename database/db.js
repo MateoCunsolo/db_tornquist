c2c15c3776baa275def58c7c33b6e8fc3b94e7bd
Mateo Cunsolo <cunsolo73@gmail.com>
2024-06-28 21:11:26 -0300

    fix connection

--- database/db.js
+++ database/db.js
@@ -1,33 +1,18 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'mysql-database-fk.alwaysdata.net',
-    user: '359795',
-    password: 'turismo_cac',
-    database: 'database-fk_tursimo'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
-//comprobar a conexion
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
+connection.connect((error) => {
+    if (error) {
+        console.error('El error de conexión es: ' + error);
         return;
     }
-    console.log('Connected as id ' + connection.threadId);
-
-    // Hacer una query a la tabla Clientes
-    connection.query('SELECT * FROM Clientes', (err, rows) => {
-        if (err) throw err;
-        console.log('Data received from Db:');
-        console.log(rows);
-
-        // cerrar la conexion
-        connection.end((err) => {
-            if (err) {
-                console.error('Error ending the connection: ' + err.stack);
-                return;
-            }
-            console.log('Connection ended.');
-        });
-    });
+    console.log('Conexión exitosa a la base de datos');
 });
+
+module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ app.use(require('./routes/comerciantes.routes'));
 app.use(require('./routes/index.routes'));
 
 
-app.listen(3000);
-console.log('Server on port 3000');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
