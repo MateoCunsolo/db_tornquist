5fe98f0e4755d8a506d4178d5b4de1ecf8b097eb
Mateo Cunsolo <cunsolo73@gmail.com>
2024-06-28 21:38:52 -0300

    add query

--- src/controllers/comerciantes.controller.js
+++ src/controllers/comerciantes.controller.js
@@ -1,16 +1,26 @@
+//conexion a la base de datos y consultas crud a la base de datos
+
+const db = require('../database/db');
+
 
 const getComerciantes = (req, res) => {
-    res.json({
-        message: 'Esta es la ruta para obtener todos los Comerciantes'
-    });
-}
-
-const createComerciantes = (req, res) => {
-    let { nombre } = req.body;
-    console.log(nombre);
-    res.json({
-        message: 'Comerciante guardado correctamente: ' + nombre,
-    });
+    try {
+        const query = 'SELECT * FROM comerciantes';
+        db.query(query, (error, rows) => {
+            if (error) {
+                console.error('El error de conexión es: ' + error);
+                return;
+            }else if(rows.length == 0){
+                res.json({
+                    message: 'No hay comerciantes registrados'
+                });
+                return;
+            }
+            res.json(rows);
+        });
+    }catch (error) {
+        console.error('El error de conexión es: ' + error);
+    }
 }
 
 module.exports = {
